ea9626429da1c53ba3004263a5548756715f3c3d
Valdis Guntis Bukalders <vgbukalders@gmail.com>
2023-01-08 15:49:26 +0200

    add more unit tests

--- __test__/probabilities.test.ts
+++ __test__/probabilities.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from '@jest/globals';
 import { ITile } from '../src/components/GameBoard';
-import { getDefaultTile, getStartingGrid } from '../src/utils/grid';
+import { getDefaultTile } from '../src/utils/grid';
 import { calculateProbabilities } from '../src/utils/probabilities';
 
 function makeTile(state: number | false): ITile {
@@ -22,13 +22,13 @@ function compareProbabilities(initialGrid: ITile[][], expectedProbabilityGrid: I
 }
 
 describe('Test probability calculation', () => {
-  test('1st', () => {
+  test('Case 1', () => {
     /**
      * H H H 1
      * H 2 2 H
      * 1 1 1 1
      */
-    const initialGrid: ITile[][] = [
+    const initialGrid = [
       [makeTile(false), makeTile(false), makeTile(false), makeTile(1)],
       [makeTile(false), makeTile(2), makeTile(2), makeTile(false)],
       [makeTile(1), makeTile(1), makeTile(1), makeTile(1)],
@@ -40,14 +40,14 @@ describe('Test probability calculation', () => {
     ];
     expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
   });
-  test('2nd', () => {
+  test('Case 2', () => {
     /**
      * H H H 1
      * 1 H H H
      * 1 H H H
      * 1 1 2 1
      */
-    const initialGrid: ITile[][] = [
+    const initialGrid = [
       [makeTile(false), makeTile(false), makeTile(false), makeTile(1)],
       [makeTile(1), makeTile(false), makeTile(false), makeTile(false)],
       [makeTile(1), makeTile(false), makeTile(false), makeTile(false)],
@@ -61,13 +61,13 @@ describe('Test probability calculation', () => {
     ];
     expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
   });
-  test('3rd', () => {
+  test('Case 3', () => {
     /**
      * H 1 H
      * 1 2 1
      * H 1 H
      */
-    const initialGrid: ITile[][] = [
+    const initialGrid = [
       [makeTile(false), makeTile(1), makeTile(false)],
       [makeTile(1), makeTile(2), makeTile(1)],
       [makeTile(false), makeTile(1), makeTile(false)],
@@ -79,13 +79,13 @@ describe('Test probability calculation', () => {
     ];
     expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
   });
-  test('4th', () => {
+  test('Case 4', () => {
     /**
      * H H H
      * H 4 H
      * H 3 H
      */
-    const initialGrid: ITile[][] = [
+    const initialGrid = [
       [makeTile(false), makeTile(false), makeTile(false)],
       [makeTile(false), makeTile(4), makeTile(false)],
       [makeTile(false), makeTile(3), makeTile(false)],
@@ -94,6 +94,114 @@ describe('Test probability calculation', () => {
       [57, 57, 57],
       [75, undefined, 75],
       [75, undefined, 75],
+    ];
+    expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
+  });
+  test('Case 5', () => {
+    /**
+     * 1 H H
+     * H 4 H
+     * H 3 H
+     */
+    const initialGrid = [
+      [makeTile(1), makeTile(false), makeTile(false)],
+      [makeTile(false), makeTile(4), makeTile(false)],
+      [makeTile(false), makeTile(3), makeTile(false)],
+    ];
+    const expectedProbabilities: Array<Array<number | undefined>> = [
+      [undefined, 67, 67],
+      [75, undefined, 75],
+      [75, undefined, 75],
+    ];
+    expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
+  });
+  test('Case 6', () => {
+    /**
+     * 2 H H
+     * H 4 H
+     * H 3 H
+     */
+    const initialGrid = [
+      [makeTile(2), makeTile(false), makeTile(false)],
+      [makeTile(false), makeTile(4), makeTile(false)],
+      [makeTile(false), makeTile(3), makeTile(false)],
+    ];
+    const expectedProbabilities: Array<Array<number | undefined>> = [
+      [undefined, 100, 50],
+      [100, undefined, 67],
+      [67, undefined, 67],
+    ];
+    expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
+  });
+  test('Case 7', () => {
+    /**
+     * 2 H 1
+     * H 3 1
+     * H 2 H
+     */
+    const initialGrid = [
+      [makeTile(2), makeTile(false), makeTile(1)],
+      [makeTile(false), makeTile(3), makeTile(1)],
+      [makeTile(false), makeTile(2), makeTile(false)],
+    ];
+    const expectedProbabilities: Array<Array<number | undefined>> = [
+      [undefined, 100, undefined],
+      [100, undefined, undefined],
+      [100, undefined, 0],
+    ];
+    expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
+  });
+  test('Case 8', () => {
+    /**
+     * 1 H H
+     * 1 2 2
+     * H H H
+     */
+    const initialGrid = [
+      [makeTile(1), makeTile(false), makeTile(false)],
+      [makeTile(1), makeTile(2), makeTile(2)],
+      [makeTile(false), makeTile(false), makeTile(false)],
+    ];
+    const expectedProbabilities: Array<Array<number | undefined>> = [
+      [undefined, 100, 50],
+      [undefined, undefined, undefined],
+      [0, 0, 50],
+    ];
+    expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
+  });
+  test('Case 9', () => {
+    /**
+     * H H H
+     * 2 3 2
+     * H H H
+     */
+    const initialGrid = [
+      [makeTile(false), makeTile(false), makeTile(false)],
+      [makeTile(2), makeTile(3), makeTile(2)],
+      [makeTile(false), makeTile(false), makeTile(false)],
+    ];
+    const expectedProbabilities: Array<Array<number | undefined>> = [
+      [50, 50, 50],
+      [undefined, undefined, undefined],
+      [50, 50, 50],
+    ];
+    expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
+  });
+  test('Case 10', () => {
+    /**
+     * H H H
+     * H 1 H
+     * H H H
+     */
+    const initialGrid = [
+      [makeTile(false), makeTile(false), makeTile(false)],
+      [makeTile(false), makeTile(1), makeTile(false)],
+      [makeTile(false), makeTile(false), makeTile(false)],
+    ];
+    const expectedProbabilities: Array<Array<number | undefined>> = [
+      [13, 13, 13],
+      [13, undefined, 13],
+      [13, 13, 13],
     ];
     expect(compareProbabilities(initialGrid, expectedProbabilities)).toBe(true);
   });
